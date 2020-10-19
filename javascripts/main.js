72f7daa0eea21e16d472eca0dcf35ecf2f850d4d
kenso11 <45929994+kenso11@users.noreply.github.com>
2020-10-19 22:21:51 +0700

    03

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,13 +11,15 @@ altitude = document.getElementById("altitude");
 var minn = 0; var secc = 0;     //นาที : วินาที
 var acceleration = 0;           //อัตตราเร่งความเร็ว
 var cumulative_speed = 0;       //ความเร็วสะสม
+var altitude_speed = 0;         //อัตตราเติมโตของความสูงจากพื้นดิน
+var altitude_cumulative = 0;    //ความสูงสะสมจากพื้นดิน
 var time_speed = 0;             //อัตตราเร่งความเร็วของเวลา
 var t_minus_speed = 0;          //อัตตราเร่งความเร็วของเวลานับถอยหลัง
 
 
 function start() {
   minn = 45; secc = 60;
-  t_minus_speed = 10;
+  t_minus_speed = 20;
   count_down();
 }
 
@@ -26,12 +28,12 @@ var i = setInterval(function interv() {
   if (time_speed != 0) {
     //       TIMER SPEED CONTROL      //
     if (parseInt(secc) == 60) { secc = 0; minn += 1 }
-    else if ((minn == 0 && parseInt(secc) == 0)) { liftoff(); acceleration = 18 }
-    else if ((minn == 0 && parseInt(secc) == 58)) { time_speed = 1.2; acceleration = 74.184; }   //SPEED     
-    else if ((minn == 2 && parseInt(secc) == 29)) { time_speed = 0.2; acceleration = 0.5;} //SLOW
-    else if ((minn == 2 && parseInt(secc) == 50)) { time_speed = 2;   acceleration = 80;}   //SPEED
-    else if ((minn == 9 && parseInt(secc) == 15)) { time_speed = 0.5; acceleration = 0.5;} //SLOW
-    else if ((minn == 9 && parseInt(secc) == 35)) { time_speed = 1.2; acceleration = 80;}   //SPEED
+    else if ((minn == 0 && parseInt(secc) == 1)) { acceleration = 18 }
+    else if ((minn == 0 && parseInt(secc) == 58)) { time_speed = 1.2; acceleration = 74.184; altitude_speed = 1; }   //SPEED     
+    else if ((minn == 2 && parseInt(secc) == 29)) { time_speed = 0.2; acceleration = 0.5; altitude_speed = 3;} //SLOW
+    else if ((minn == 2 && parseInt(secc) == 50)) { time_speed = 2; acceleration = 80; altitude_speed = 4;}   //SPEED
+    else if ((minn == 8 && parseInt(secc) == 46)) { time_speed = 0.2; acceleration = 0.5; altitude_speed = 5;} //SLOW
+    else if ((minn == 9 && parseInt(secc) == 35)) { time_speed = 1.2; acceleration = 80; altitude_speed = 6;}   //SPEED
   
 
     //   CONTROL TWEEN AND TIMELINE    //
@@ -41,15 +43,15 @@ var i = setInterval(function interv() {
     else if ((minn == 2 && parseInt(secc) == 44)) { start_2nd(); }
     else if ((minn == 7 && parseInt(secc) == 10)) { entry_burn_1st(); }
     else if ((minn == 8 && parseInt(secc) == 47)) { seco_1(); }
-    else if ((minn == 9 && parseInt(secc) == 22)) { landing_1st(); }
+    else if ((minn == 9 && parseInt(secc) == 0)) { landing_1st(); }
     else if ((minn == 12 && parseInt(secc) == 0)) { separation_2nd(); }
   
   }
 
   secc += time_speed;
   cumulative_speed += acceleration;
-
   speed.innerHTML = cumulative_speed;
+  altitude.innerHTML = altitude_cumulative;
   min.innerHTML = minn;
   sec.innerHTML = parseInt(secc);
 }, 100);
@@ -58,14 +60,14 @@ var i = setInterval(function interv() {
 
 CustomEase.create("hop", "M0,0 C0.176,0.056 0.306,-0.028 0.616,0.244 0.661,0.283 0.798,0.468 1,0.568");
 TweenMax.to("body", {background:"linear-gradient(to right, #f00, #0f0, #00f, #600, #060)"});
-gsap.to(".circle", { rotation: 10});
-gsap.to(".status_footer", { opacity: 1, duration: 2 });
-
 
 //       COUNT_DOWN
 function count_down() {
+  document.getElementById("time_status").innerHTML = "นับถอยหลัง";
+  gsap.to(".t_minus", { display: 'block', duration: 1 });
   gsap.to(".timeline_box", { xPercent: 13, yPercent: 50 });
   var k = setInterval(function interv() {
+    secc -= t_minus_speed;
     if (t_minus_speed != 0) {
       if (parseInt(secc) == 0) { secc = 60; minn -= 1; }
       //   CONTROL TWEEN AND TIMELINE    //
@@ -85,28 +87,40 @@ function count_down() {
         gsap.to(".tl_4", { display: 'block', duration: 1, text: "T-minus: 01:00 minute – Pre-launch Checks (ตรวจสอบขั้นสุดท้ายก่อนการปล่อย) และเริ่มการอัดความดันในถังเชื้อเพลิง (Propellant Tank Pressurization) <br> T-minus: 00:45 minute – Launch director ให้สัญญาณ Go/No Go" });
       }
       else if (minn == 0) {
-        gsap.to(".timeline_box", { display: 'none', duration: 1 });
-        clearInterval(k); minn = 0; secc = 0; time_speed = 1;
+        minn = 0;
+        secc = 0;
+        document.getElementById("time_status").innerHTML = "";
+        clearInterval(k);
+        go_nogo();
       }
     }
     min.innerHTML = minn;
     sec.innerHTML = parseInt(secc);
-    secc -= t_minus_speed;
 }, 100);
  }
 
 //       GO / NO GO
+function return_go() {
+  liftoff();
+  gsap.to(".go_nogo", { display: 'none', duration: 1 });
+  gsap.to(".timeline_box", { display: 'none', duration: 1 });
+};
+function return_nogo() { time_speed = 0; };
 function go_nogo() {
-  
+  setTimeout(function () { return_go() }, 9000);
+  gsap.to(".timeout_1", { width: '100%', duration: 10 });
+  gsap.to(".go_nogo", { display: 'block', opacity: 1, duration: 1 });
 }
 
 //       LIFTOFF
 function liftoff() {
+  time_speed = 1;
+  gsap.to(".t_minus ", {top: '90%', left:'48%', color:'#fff'});
   gsap.to(".circle", { rotation: -29.5, duration: 7 });
-  setTimeout(function () { gsap.to(".status_footer", { opacity: 1, duration: 2 }); }, 2000);
-  setTimeout(function () { gsap.to(".dragon", { rotation: 10, duration: 5 }); }, 4000);
-  setTimeout(function () { gsap.to(".dragon", { rotation: 40, duration: 10 }); }, 6000);
-  setTimeout(function () { gsap.to(".dragon", { rotation: 85,  duration: 10 }); }, 9000);
+  setTimeout(function () {
+    gsap.to(".left", { opacity: 1, duration: 1 });
+    gsap.to(".right", { opacity: 1, duration: 1 });  }, 2000);
+  setTimeout(function () { gsap.to(".dragon", { rotation: 85,  duration: 15 }); }, 7000);
   setTimeout(function () { gsap.to(".eart", { top: '-250%', duration: 10 }); }, 10000);
   setTimeout(function () { gsap.to(".eart", { rotation: -100, duration: 40 }); }, 11000);
 
@@ -124,37 +138,44 @@ function liftoff() {
 function max_q() {
   gsap.to(".circle", { rotation: -51.5,duration: 11.5});
   gsap.to(".tl_1", { display: 'block', duration: 0.5, text: "00:00:58 MAX-Q" });
-}
-
-//       MECO
-function meco() {
-  gsap.to(".circle", { rotation: -58.4,duration: 10});
-  gsap.to(".tl_2", { display: 'block', duration: 0.5, text: "00:02:33 MECO" });
   gsap.to(".parallax", { scale: 0.7, xPercent: 0, duration: 3 });
   gsap.to(".eart_iss", { scale: 0.7, xPercent: 0, duration: 3 });
 }
 
+//       MECO
+function meco() {
+  gsap.to(".tl_2", { display: 'block', duration: 0.5, text: "00:02:33 MECO" });
+}
+
 //       1ST SEPARATION
+function return_separation_1st() {
+  gsap.to(".dragon ._1st", { top: 10000, duration: 5, ease: "hop" })
+  gsap.to(".separation", { opacity: 0, display: 'none', duration: 1 });
+}
 function separation_1st() {
-  gsap.to(".dragon ._1st", { top: 7000, xPercent: 10, duration: 10, ease: "hop" })
+  setTimeout(function () { return_separation_1st() }, 9000);
+  gsap.to(".parallax", { scale: 1, xPercent: 0, duration: 3 });
+  gsap.to(".eart_iss", { scale: 0.2, xPercent: 0, duration: 3 });
+  gsap.to(".separation", { opacity: 1, display: 'block', duration: 1 });
+  gsap.to(".timeout_2", { width: '100%', duration: 10 });
 }
 
 //       2ND START
 function start_2nd() {
-  gsap.to(".circle", { rotation: -97.18,duration: 18.5});
-  gsap.to(".parallax", { scale: 0.2, xPercent: 0, duration: 5 });
-  gsap.to(".eart_iss", { scale: 1, xPercent: 0, duration: 5 });
+  gsap.to(".circle", { rotation: -97.18, duration: 20 });
+    gsap.to(".parallax", { scale: 0.2, xPercent: 0, duration: 3 });
+    gsap.to(".eart_iss", { scale: 1, xPercent: 0, duration: 3 });
 }
 //       1ST ENTRY BURN
 function entry_burn_1st() {
-  gsap.to(".circle", { rotation: -118.18,duration: 10.5});
+  gsap.to(".circle", { rotation: -118.18,duration: 10});
   tl.to(".landing_1", { display:'block', duration: 5 })
     .to(".landing_2", { display:'block', duration: 2 });
 }
 
 //       SECO-1
 function seco_1() {
-  gsap.to(".circle", { rotation: -120.65, duration: 2});
+  gsap.to(".circle", { rotation: -120.65, duration: 6});
  }
 
 //       1ST LANDING
